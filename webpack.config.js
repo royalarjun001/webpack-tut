3e3a95fbd81871c1f745e599e3d307aa5fb5364d
Arjun Verma <royalarjun001@gmail.com>
2021-12-18 14:40:09 +0530

    create empty webpack file

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //  this is configuration setting which will be now in used by
+    //  web pack.
+    //
+    //  we can change the default webpack.config.js filte name to some
+    //  thing else like mywebpack.js to able to work with these changes
+    //  we need to pass the --config mywebpack.js with the default webpack
+    //  command other wise it will look for the default file name which is
+    //  webpack.config.js
+}
